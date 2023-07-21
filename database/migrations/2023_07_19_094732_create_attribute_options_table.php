c29f8ea4fb63c8c1e5897cc45417ba5fb62df002
modestasv <info@modestasv.lt>
2023-07-21 08:41:02 +0300

    Imported class

--- database/migrations/2023_07_19_094732_create_attribute_options_table.php
+++ database/migrations/2023_07_19_094732_create_attribute_options_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Attribute;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -9,7 +10,7 @@ return new class extends Migration {
     {
         Schema::create('attribute_options', static function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\Attribute::class)->constrained();
+            $table->foreignIdFor(Attribute::class)->constrained();
             $table->string('value');
             $table->timestamps();
         });
